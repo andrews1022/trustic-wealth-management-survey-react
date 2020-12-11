c9c35c05ad4aaac2f3273ca44149350b92232563
andrews1022 <andrewshearerphotography@gmail.com>
2020-12-10 21:58:13 -0800

    added styles and masterform component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import MasterForm from './components/MasterForm';
+import './styles/app.min.css';
 
 const App = () => {
 	return (
 		<div className='app'>
 			<h1>Trustic Wealth Management Survey</h1>
+			<MasterForm />
 		</div>
 	);
 };
---
+++ src/components/MasterForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MasterForm = () => {
+	return (
+		<div>
+			<h2>Hello from MasterForm</h2>
+		</div>
+	);
+};
+
+export default MasterForm;
