1f4b3a24f5af2b6b62be5c1fc62b3969f36652a9
andrews1022 <andrewshearerphotography@gmail.com>
2021-02-18 21:53:17 -0800

    separated svgs into separate components and start converting to context for state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import React, { createContext, useReducer } from 'react';
 import MasterForm from './components/MasterForm';
 import './styles/app.min.css';
 
+export const FormContext = createContext();
+
+const initialFormState = {
+	currentStep: 0,
+	currentQuestion: 1,
+	isSimpleModalOpen: false,
+	isIntroductoryCallModalOpen: false,
+	simpleModalHeadingText: '',
+	checkedOptions: []
+};
+
+const formReducer = (state, action) => {
+	switch (action.type) {
+		case 'INCREMENT_CURRENT_STEP':
+			return {
+				...state,
+				currentStep: state.currentStep + 1
+			};
+
+		case 'INCREMENT_CURRENT_STEP_AND_QUESTION':
+			return {
+				...state,
+				currentStep: state.currentStep + 1,
+				currentQuestion: state.currentQuestion + 1
+			};
+
+		default:
+			return {
+				...state
+			};
+	}
+};
+
 const App = () => {
+	const [state, dispatch] = useReducer(formReducer, initialFormState);
+
 	return (
 		<div className='app'>
-			<MasterForm />
+			<FormContext.Provider value={{ formState: state, formDispatch: dispatch }}>
+				<MasterForm />
+			</FormContext.Provider>
 		</div>
 	);
 };
